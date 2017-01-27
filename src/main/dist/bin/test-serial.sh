4bb1d24b0b301cbd531dec8b5429b4c6c1c60bb3
Romanas Sonkinas <romanass@gmail.com>
2017-01-27 22:51:38 +0000

    Scripts

---
+++ src/main/dist/bin/test-serial.sh
@@ -0,0 +1,1 @@
+java -cp lib/commons-lang3-3.2.1.jar:lib/commons-net-3.3.jar:lib/nrjavaserial-3.11.0.jar:lib/skelmir-tests-0.1-SNAPSHOT.jar io.imont.sdk.skelmir.SerialMain
--- src/main/dist/bin/test-sqlite.sh
+++ src/main/dist/bin/test-sqlite.sh
@@ -1,1 +1,1 @@
-java -scp lib/skelmir-tests-0.1-SNAPSHOT.jar:lib/sqlite-jdbc-3.8.11.2.jar io.imont.sdk.skelmir.SQLiteMain
+java -cp lib/skelmir-tests-0.1-SNAPSHOT.jar:lib/sqlite-jdbc-3.8.11.2.jar io.imont.sdk.skelmir.SQLiteMain
